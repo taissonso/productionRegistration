54d8717a9d88a491129bb7089d221aee594bc513
Taisson <taisson.oliveira@gmail.com>
2020-07-02 15:31:23 -0300

    Feito a validação dos campos Preço e Produto Perecível, assim como a ocultação ou não do campo de validade

--- _js/validadores.js
+++ _js/validadores.js
@@ -1,27 +1,32 @@
+//Carrega a página e desativa o botão de enviar formulário, para evitar o envio do formulário vazio
+window.onload = enviaDados;
+
 var form = document.getElementsByTagName('form')[0];
 
+//Pega os elementos do HMTL para usar nos eventos
 var nomeForm = document.getElementById('nome');
 var medidaForm = document.getElementById('medida');
 var quantidadeForm = document.getElementById('quantidade');
+var precoForm = document.getElementById('preco');
+var perecivelForm = document.getElementById('perecivel');
 
 var btnEnviar = document.getElementById('enviar');
 var btnCancelar = document.getElementById('resetar');
-
-//Carrega o arquivo e desativa o botão de enviar 
-form.addEventListener('load', botaoEnviar(0));
+var btnLimpar = document.getElementById('limpar');
 
 //Eventos para os campos e botões
 nomeForm.addEventListener('keyup', validaNome);
 medidaForm.addEventListener('change', validaMedida);
 quantidadeForm.addEventListener('keyup', validaQuantidade);
+precoForm.addEventListener('keyup', validaPreco);
+perecivelForm.addEventListener('click', validaPerecivel);
 
+fabricacaoForm.addEventListener('change', validaFabricacao)
 btnCancelar.addEventListener('click', cancelaEnvio);
-
 btnEnviar.addEventListener('click', enviaDados);
-
+btnLimpar.addEventListener('click', limparForm);
 //Funções
 function validaNome() {
-    //Pega o valor do campo nome
     var nome = document.getElementById('nome').value;
     //define uma regex
     var regex = new RegExp(/[^a-zA-Z\s+]/);
@@ -41,7 +46,7 @@ function validaNome() {
 //Função que pega o valor da Unidade de Medida
 //Primeiro pega o id do select
 //Depois pega o valor do option usando a variavel do select com .selectedIndex 
-//Pode pegar o valor do value(variavel_option.value) ou do texto (variavel_option.text)
+//Pode pegar o valor do value ou do texto
 function validaMedida() {
     var seleciona = document.getElementById('medida');
     var medida = seleciona.options[seleciona.selectedIndex].value;
@@ -63,15 +68,15 @@ function validaQuantidade(medida) {
     medidaForm.addEventListener('change', validaMedida);
     var seleciona = document.getElementById('medida');
     var aux = seleciona.options[seleciona.selectedIndex].value;
-    var regex1 = new RegExp(/\D+/);
+    var regex = new RegExp(/\D+/);
 
     var quantidade = document.getElementById('quantidade').value;
-    console.log('DIGITADO: ' + quantidade + ' RESULTADO: ' + regex1.test(quantidade));
-    
-    if (!regex1.test(quantidade) || quantidade === '') {
+    console.log('DIGITADO: ' + quantidade + ' RESULTADO: ' + regex.test(quantidade));
+
+    if (!regex.test(quantidade) || quantidade == '') {
         document.getElementById('erroQuantidade').innerHTML = '';
         if (aux != '1') {
-            document.getElementById('erroQuantidade').innerHTML ='';
+            document.getElementById('erroQuantidade').innerHTML = '';
             if (aux == 'un') {
                 console.log('CONFIGURAR COMO UNIDADE');
             } else {
@@ -81,7 +86,7 @@ function validaQuantidade(medida) {
             document.getElementById('erroMedida').innerHTML = '* Por Favor, selecione uma opção!';
         }
     } else {
-        if(quantidade.trim() === ''){
+        if (quantidade.trim() == '') {
             document.getElementById('erroQuantidade').innerHTML = '* Campo vazio!';
         } else {
             document.getElementById('erroQuantidade').innerHTML = '* Campo obrigatorio, não aceita letras!';
@@ -89,13 +94,42 @@ function validaQuantidade(medida) {
     }
 }
 
-//Função que fica controlando o botão enviar só para fins de testes
-function botaoEnviar(flag) {
-    if (flag == 0) {
-        document.getElementById('enviar').onclick = function () { return false }
+function validaPreco() {
+    var preco = document.getElementById('preco').value;
+    var regex = new RegExp(/\D+/);
+
+    if (!regex.test(preco) || preco == '') {
+        document.getElementById('erroPreco').innerHTML = '';
+        //mascara para preço
     } else {
-        document.getElementById('enviar').onclick = function () { return true }
+        if (preco.trim() == '') {
+            document.getElementById('erroPreco').innerHTML = '* Campo vazio!';
+        } else {
+            document.getElementById('erroPreco').innerHTML = '* Campo obrigatorio, não aceita letras ou espaço em branco!';
+        }
     }
+}
+
+//Verifica se o checkbox foi selecionado, se sim mostra o campo de data de validade 
+function validaPerecivel() {
+    var perecivel = document.getElementById('perecivel');
+
+    if (perecivel.checked == true) {
+        document.getElementById('label').style.display = 'inline';
+        document.getElementById('validade').style.display = 'inline';
+        return perecivel = 'Sim';
+    } else {
+        //se não for perecivel então esconde a data 
+        document.getElementById('label').style.display = 'none';
+        document.getElementById('validade').style.display = 'none';
+        return perecivel = 'Não';
+    }
+}
+
+//Valida a data de fabricação do produto 
+function validaFabricacao () {
+    var data = document.getElementById('fabricacao').value;
+    console.log('Data adquirida: ' + data);
 }
 
 //Como é um botão com reset ele limpa o formulário e desativa o botão de enviar
@@ -105,24 +139,42 @@ function cancelaEnvio() {
     document.getElementById('erroMedida').innerHTML = ''
     document.getElementById('medidadeUnidade').innerHTML = '';
     document.getElementById('erroQuantidade').innerHTML = '';
+    document.getElementById('erroPreco').innerHTML = '';
     document.getElementById('enviar').onclick = function () { return false }
 }
 
-//Para colocar no localStorage e mostrar na janela Modal
+function limparForm() {
+    document.getElementById('nome').value = '';
+    document.getElementById('medida').selectedIndex = 0;
+    document.getElementById('medidadeUnidade').innerHTML = '';
+    document.getElementById('quantidade').value = '';
+    document.getElementById('preco').value = '';
+    document.getElementById('perecivel').checked = false;
+    document.getElementById('label').style.display = 'none';
+    document.getElementById('validade').style.display = 'none';
+}
+
+//Para colocar no localStorage e mostrar na janela Modal, também desativa o botão enviar formulário
+//ao carregar a página via CSS
 function enviaDados() {
+
     //localStorage.setItem('nome', nome);
     //localStorage.setItem('medida', medidaTexto);
+
     var nome = document.getElementById('nome').value;
     var seleciona = document.getElementById('medida');
-    var medida = seleciona.options[seleciona.selectedIndex].value;
     var medidaTexto = seleciona.options[seleciona.selectedIndex].text;
     var quantidade = document.getElementById('quantidade').value;
-    if (nome != '' && medida != '1' && quantidade != '') {
-        document.getElementById('enviar').onclick = true;
+    var preco = document.getElementById('preco').value;
+    var perecivel = validaPerecivel();
+    if (nome != '' && preco != '') {
+        document.getElementById('enviar').onclick = function () { return true }
         var modal = document.getElementById('escreveModal');
         modal.innerHTML = "<p> Nome do Produto: " + nome + "</p><br>"
             + "<p> Unidade de Medida: " + medidaTexto + "</p><br>"
-            + "<p> Quantidade: " + quantidade + "</p>";
+            + "<p> Quantidade: " + quantidade + "</p><br>"
+            + "<p> Preço: R$ " + preco + "</p><br>"
+            + "<p> Perecivel: " + perecivel + "</p><br>";
     } else {
         document.getElementById('enviar').onclick = function () { return false }
     }
